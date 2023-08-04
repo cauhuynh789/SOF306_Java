b7c99118bc8a4a99cb493ffca2f385b16e26ee8b
cauhuynh789 <114749842+cauhuynh789@users.noreply.github.com>
2023-08-04 22:40:17 +0700

    thien

--- SOF306_ASM/src/main/resources/static/assets/admin/authors/authors.js
+++ SOF306_ASM/src/main/resources/static/assets/admin/authors/authors.js
@@ -3,76 +3,116 @@ const app = angular.module("app", []);
 app.controller("ctrl", ($scope, $http) => {
     $scope.form = {};
     $scope.items = [];
+
     $scope.reset = () => {
-        $scope.form = {gender: true, photo: "default.jpg", status: true, roleId: true};
-    }
-    $scope.load_all = () => {
-        var url = `${host}/accounts`    ;
-        $http.get(url).then(resp => {
-            $scope.items = resp.data;
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    }
-    $scope.edit = (accountId) => {
-        var url = `${host}/accounts/${accountId}`;
-        $http.get(url).then(resp => {
-            $scope.form = resp.data;
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    }
+        $scope.form = {};
+    };
+    $scope.load_all = function() {
+        var url = `${host}/authors`;
+        $http
+            .get(url)
+            .then((resp) => {
+                $scope.items = resp.data;
+                console.log("Success", resp);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
+
+    $scope.edit = (authorId) => {
+        var url = `${host}/authors/${authorId}`;
+        $http
+            .get(url)
+            .then((resp) => {
+                $scope.form = resp.data;
+                console.log("Success", resp);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
     $scope.create = () => {
         var item = angular.copy($scope.form);
-        var url = `${host}/accounts`;
-        $http.post(url, item).then(resp => {
-            $scope.items.push(item);
-            $scope.reset();
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });        
-    }
+        var url = `${host}/authors`;
+        $http
+            .post(url, item)
+            .then((resp) => {
+                $scope.items.push(item);
+                $scope.reset();
+                console.log("Success", resp);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
     $scope.update = () => {
         var item = angular.copy($scope.form);
-        var url = `${host}/accounts/${$scope.form.accountId}`;
-        $http.put(url, item).then(resp => {
-            var index = $scope.items.findIndex(item => item.accountId == $scope.form.accountId);
-            $scope.items[index] = item;
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    }
-    $scope.delete = (accountId) => {
-        var url = `${host}/accounts/${$scope.form.accountId}`;
-        $http.delete(url).then(resp => {
-            var index = $scope.items.findIndex(item => item.accountId == $scope.form.accountId);
-            $scope.items.splice(index, 1);
-            $scope.reset();
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    }
+        var url = `${host}/authors/${$scope.form.authorId}`;
+        $http
+            .put(url, item)
+            .then((resp) => {
+                var index = $scope.items.findIndex(
+                    (item) => item.authorId == $scope.form.authorId
+                );
+                $scope.items[index] = resp.data;
+                console.log("Success", resp);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
 
-    $scope.imageChanged = function (files) {
+    $scope.delete = function(authorId) {
+        var url = `${host}/authors/${$scope.form.authorId}`;
+        $http
+            .delete(url)
+            .then((resp) => {
+                var index = $scope.items.findIndex(
+                    (item) => item.authorId == $scope.form.authorId
+                );
+                $scope.items.splice(index, 1);
+                $scope.reset();
+                console.log("Success", resp);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
+    // $scope.delete = (authorId) => {
+    //     var url = `${host}/authors/${$scope.form.authorId}`;
+    //     $http
+    //         .delete(url)
+    //         .then((resp) => {
+    //             var index = $scope.items.findIndex(
+    //                 (item) => item.authorId == $scope.form.authorId
+    //             );
+    //             $scope.items.splice(index, 1);
+    //             $scope.reset();
+    //             console.log("Success", resp);
+    //         })
+    //         .catch((error) => {
+    //             console.log("Error", error);
+    //         });
+    // };
+
+    $scope.imageChanged = function(files) {
         var data = new FormData();
-        data.append('file', files[0]);
-        $http.post('/rest/upload/images', data, {
-            transformRequest: angular.identity,
-            headers: { 'Content-Type': undefined }
-        }).then(resp => {
-            $scope.form.image = resp.data.name;
-        }).catch(error => {
-            alert("Upload loi");
-            console.log(error);
-        })
-    }
+        data.append("file", files[0]);
+        $http
+            .post("/rest/upload/images", data, {
+                transformRequest: angular.identity,
+                headers: { "Content-Type": undefined },
+            })
+            .then((resp) => {
+                $scope.form.image = resp.data.name;
+            })
+            .catch((error) => {
+                alert("Upload loi");
+                console.log(error);
+            });
+    };
 
     $scope.load_all();
     $scope.reset();
 });
-

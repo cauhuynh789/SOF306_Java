9d089286442c90ddc8411d04ad1b12404c1657da
cauhuynh789 <114749842+cauhuynh789@users.noreply.github.com>
2023-08-04 23:23:00 +0700

    thien

--- SOF306_ASM/src/main/resources/static/assets/admin/admin-app.js
+++ SOF306_ASM/src/main/resources/static/assets/admin/admin-app.js
@@ -1,23 +1,23 @@
 const app = angular.module("admin-app", ["ngRoute"]);
-app.config($routeProvider => {
-    $routeProvider
-        .when("/accounts", {
-            templateUrl: "/assets/admin/accounts/index.html",
-            controller: "accounts-ctrl"
-        })
-        .when("/books", {
-            templateUrl: "/assets/admin/books/index.html",
-            controller: "books-ctrl"
-        })
-        .when("/authorize", {
-            templateUrl: "/assets/admin/authors/index.html",
-            controller: "authors-ctrl"
-        })
-        .when("/unauthorized", {
-            templateUrl: "/assets/admin/authority/unauthorized.html",
-            controller: "authority-ctrl"
-        })
-        .otherwise({
-            template: "<h1 class='text-center'>Administration</h1>"
-        });
+app.config(($routeProvider) => {
+  $routeProvider
+    .when("/accounts", {
+      templateUrl: "/assets/admin/accounts/index.html",
+      controller: "accounts-ctrl",
+    })
+    .when("/books", {
+      templateUrl: "/assets/admin/books/index.html",
+      controller: "books-ctrl",
+    })
+    .when("/authorize", {
+      templateUrl: "/assets/admin/authors/authors.html",
+      controller: "authority-ctrl",
+    })
+    .when("/unauthorized", {
+      templateUrl: "/assets/admin/authority/unauthorized.html",
+      controller: "authority-ctrl",
+    })
+    .otherwise({
+      template: "<h1 class='text-center'>Administration</h1>",
+    });
 });
--- SOF306_ASM/src/main/resources/static/assets/admin/authors/authors.js
+++ SOF306_ASM/src/main/resources/static/assets/admin/authors/authors.js
@@ -1,118 +1,118 @@
 let host = "http://localhost:8080/rest";
 const app = angular.module("app", []);
-app.controller("authors-ctrl", ($scope, $http) => {
+app.controller("authority-ctrl", ($scope, $http) => {
+  $scope.form = {};
+  $scope.items = [];
+
+  $scope.reset = () => {
     $scope.form = {};
-    $scope.items = [];
+  };
+  $scope.load_all = function () {
+    var url = `${host}/authors`;
+    $http
+      .get(url)
+      .then((resp) => {
+        $scope.items = resp.data;
+        console.log("Success", resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
 
-    $scope.reset = () => {
-        $scope.form = {};
-    };
-    $scope.load_all = function() {
-        var url = `${host}/authors`;
-        $http
-            .get(url)
-            .then((resp) => {
-                $scope.items = resp.data;
-                console.log("Success", resp);
-            })
-            .catch((error) => {
-                console.log("Error", error);
-            });
-    };
+  $scope.edit = (authorId) => {
+    var url = `${host}/authors/${authorId}`;
+    $http
+      .get(url)
+      .then((resp) => {
+        $scope.form = resp.data;
+        console.log("Success", resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+  $scope.create = () => {
+    var item = angular.copy($scope.form);
+    var url = `${host}/authors`;
+    $http
+      .post(url, item)
+      .then((resp) => {
+        $scope.items.push(item);
+        $scope.reset();
+        console.log("Success", resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+  $scope.update = () => {
+    var item = angular.copy($scope.form);
+    var url = `${host}/authors/${$scope.form.authorId}`;
+    $http
+      .put(url, item)
+      .then((resp) => {
+        var index = $scope.items.findIndex(
+          (item) => item.authorId == $scope.form.authorId
+        );
+        $scope.items[index] = resp.data;
+        console.log("Success", resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
 
-    $scope.edit = (authorId) => {
-        var url = `${host}/authors/${authorId}`;
-        $http
-            .get(url)
-            .then((resp) => {
-                $scope.form = resp.data;
-                console.log("Success", resp);
-            })
-            .catch((error) => {
-                console.log("Error", error);
-            });
-    };
-    $scope.create = () => {
-        var item = angular.copy($scope.form);
-        var url = `${host}/authors`;
-        $http
-            .post(url, item)
-            .then((resp) => {
-                $scope.items.push(item);
-                $scope.reset();
-                console.log("Success", resp);
-            })
-            .catch((error) => {
-                console.log("Error", error);
-            });
-    };
-    $scope.update = () => {
-        var item = angular.copy($scope.form);
-        var url = `${host}/authors/${$scope.form.authorId}`;
-        $http
-            .put(url, item)
-            .then((resp) => {
-                var index = $scope.items.findIndex(
-                    (item) => item.authorId == $scope.form.authorId
-                );
-                $scope.items[index] = resp.data;
-                console.log("Success", resp);
-            })
-            .catch((error) => {
-                console.log("Error", error);
-            });
-    };
+  $scope.delete = function (authorId) {
+    var url = `${host}/authors/${$scope.form.authorId}`;
+    $http
+      .delete(url)
+      .then((resp) => {
+        var index = $scope.items.findIndex(
+          (item) => item.authorId == $scope.form.authorId
+        );
+        $scope.items.splice(index, 1);
+        $scope.reset();
+        console.log("Success", resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+  // $scope.delete = (authorId) => {
+  //     var url = `${host}/authors/${$scope.form.authorId}`;
+  //     $http
+  //         .delete(url)
+  //         .then((resp) => {
+  //             var index = $scope.items.findIndex(
+  //                 (item) => item.authorId == $scope.form.authorId
+  //             );
+  //             $scope.items.splice(index, 1);
+  //             $scope.reset();
+  //             console.log("Success", resp);
+  //         })
+  //         .catch((error) => {
+  //             console.log("Error", error);
+  //         });
+  // };
 
-    $scope.delete = function(authorId) {
-        var url = `${host}/authors/${$scope.form.authorId}`;
-        $http
-            .delete(url)
-            .then((resp) => {
-                var index = $scope.items.findIndex(
-                    (item) => item.authorId == $scope.form.authorId
-                );
-                $scope.items.splice(index, 1);
-                $scope.reset();
-                console.log("Success", resp);
-            })
-            .catch((error) => {
-                console.log("Error", error);
-            });
-    };
-    // $scope.delete = (authorId) => {
-    //     var url = `${host}/authors/${$scope.form.authorId}`;
-    //     $http
-    //         .delete(url)
-    //         .then((resp) => {
-    //             var index = $scope.items.findIndex(
-    //                 (item) => item.authorId == $scope.form.authorId
-    //             );
-    //             $scope.items.splice(index, 1);
-    //             $scope.reset();
-    //             console.log("Success", resp);
-    //         })
-    //         .catch((error) => {
-    //             console.log("Error", error);
-    //         });
-    // };
+  $scope.imageChanged = function (files) {
+    var data = new FormData();
+    data.append("file", files[0]);
+    $http
+      .post("/rest/upload/images", data, {
+        transformRequest: angular.identity,
+        headers: { "Content-Type": undefined },
+      })
+      .then((resp) => {
+        $scope.form.image = resp.data.name;
+      })
+      .catch((error) => {
+        alert("Upload loi");
+        console.log(error);
+      });
+  };
 
-    $scope.imageChanged = function(files) {
-        var data = new FormData();
-        data.append("file", files[0]);
-        $http
-            .post("/rest/upload/images", data, {
-                transformRequest: angular.identity,
-                headers: { "Content-Type": undefined },
-            })
-            .then((resp) => {
-                $scope.form.image = resp.data.name;
-            })
-            .catch((error) => {
-                alert("Upload loi");
-                console.log(error);
-            });
-    };
-
-    $scope.load_all();
-    $scope.reset();
+  $scope.load_all();
+  $scope.reset();
 });
